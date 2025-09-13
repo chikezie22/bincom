6688f203de43c3a11a3ae6ba18e7192a9d4babd3
Chikezie Simon <simonchikezie44@gmail.com>
2025-09-13 13:10:58 +0100

    fix railway db

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ app.use(express.json());
 
 // connect to DB
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('DB connection failed:', err);
-    return;
+    console.error("DB connection failed:", err);
+  } else {
+    console.log("✅ Connected to Railway MySQL!");
   }
-  console.log('✅ MySQL connected');
 });
 
 // === ROUTES ===
